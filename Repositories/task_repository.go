3b5912b9c914d41d229a8e95a3dfeb645426ee73
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-08-15 00:16:39 +0300

    finished testing task_repository

--- Repositories/task_repository.go
+++ Repositories/task_repository.go
@@ -12,8 +12,8 @@ import (
 
 // taskRepository represents a repository for managing tasks.
 type taskRepository struct {
-    database   mongo.Database
-    collection string
+	database   mongo.Database
+	collection string
 }
 
 // NewTaskRepository creates a new instance of the TaskRepository interface.
@@ -29,25 +29,25 @@ func NewTaskRepository(db mongo.Database, collection string) domain.TaskReposito
 // FindAlltasks retrieves all tasks from the task repository.
 // It returns a slice of domain.Task and an error if any.
 func (tr *taskRepository) FindAlltasks(ctx context.Context) ([]domain.Task, error) {
-    collection := tr.database.Collection(tr.collection)
-    var tasks []domain.Task
-    cursor, err := collection.Find(ctx, bson.D{})
-    if err != nil {
-        return nil, err
-    }
-    defer cursor.Close(ctx)
+	collection := tr.database.Collection(tr.collection)
+	var tasks []domain.Task
+	cursor, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
 
-    for cursor.Next(ctx) {
-        var task domain.Task
-        if err := cursor.Decode(&task); err != nil {
-            return nil, err
-        }
-        tasks = append(tasks, task)
-    }
-    if err := cursor.Err(); err != nil {
-        return nil, err
-    }
-    return tasks, nil
+	for cursor.Next(ctx) {
+		var task domain.Task
+		if err := cursor.Decode(&task); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, task)
+	}
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+	return tasks, nil
 }
 
 // FindTaskById retrieves a task from the database by its ID.
@@ -55,80 +55,78 @@ func (tr *taskRepository) FindAlltasks(ctx context.Context) ([]domain.Task, erro
 // It returns a domain.Task and an error.
 // The domain.Task represents the retrieved task from the database.
 // The error is returned if there was an issue retrieving the task.
-func (tr *taskRepository) FindTaskById(ctx context.Context, taskId string) (domain.Task, error){
+func (tr *taskRepository) FindTaskById(ctx context.Context, taskId string) (domain.Task, error) {
 	collection := tr.database.Collection(tr.collection)
 	objID, err := primitive.ObjectIDFromHex(taskId)
-	if err != nil{
+	if err != nil {
 		return domain.Task{}, err
 	}
 	filter := bson.M{"_id": objID}
-	
+
 	var task domain.Task
-    err = collection.FindOne(ctx, filter).Decode(&task)
-    if err != nil {
-        if err == mongo.ErrNoDocuments {
-            return domain.Task{}, err // No document found
-        }
-        return domain.Task{}, err
-    }
-    return task, nil
+	err = collection.FindOne(ctx, filter).Decode(&task)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return domain.Task{}, err // No document found
+		}
+		return domain.Task{}, err
+	}
+	return task, nil
 }
 
 // CreateTask creates a new task in the task repository.
 // It takes a context and a task object as parameters.
 // It returns the created task and an error, if any.
-func (tr *taskRepository) CreateTask(ctx context.Context, task domain.Task) (domain.Task, error){
+func (tr *taskRepository) CreateTask(ctx context.Context, task domain.Task) (domain.Task, error) {
 	collection := tr.database.Collection(tr.collection)
 	task.ID = primitive.NewObjectID()
-    // Insert the task into the collection
-    _, err := collection.InsertOne(ctx, task)
-    if err != nil {
-        return domain.Task{}, err
+	// Insert the task into the collection
+	_, err := collection.InsertOne(ctx, task)
+	if err != nil {
+		return domain.Task{}, err
 	}
-    return task, nil
+	return task, nil
 }
 
 // UpdateTaskById updates a task in the database with the given ID.
 // It takes the updatedTask object containing the new values for the task fields,
 // the id string representing the ID of the task to be updated.
 // It returns the updated task object and an error if any occurred.
-func (tr *taskRepository) UpdateTaskById(ctx context.Context, updatedTask domain.Task, id string) (domain.Task, error){
+func (tr *taskRepository) UpdateTaskById(ctx context.Context, updatedTask domain.Task, id string) (domain.Task, error) {
 	collection := tr.database.Collection(tr.collection)
 	update := bson.M{
-        "$set": bson.M{
-            "title":       updatedTask.Title,
-            "description": updatedTask.Description,
-            "due_date":    updatedTask.DueDate,
-            "status":      updatedTask.Status,
-        },
-    }
-
-    filter := bson.M{"_id": id}
-    opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
-    var updated domain.Task
-    err := collection.FindOneAndUpdate(ctx, filter, update, opts).Decode(&updated)
-    if err != nil {
-        if err == mongo.ErrNoDocuments {
-            return domain.Task{}, err
-        }
-        return domain.Task{}, err
-    }
-    return updated, nil
+		"$set": bson.M{
+			"title":       updatedTask.Title,
+			"description": updatedTask.Description,
+			"due_date":    updatedTask.DueDate,
+			"status":      updatedTask.Status,
+		},
+	}
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return domain.Task{}, err
+	}
+	filter := bson.M{"_id": objID}
+	opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
+	var updated domain.Task
+	err = collection.FindOneAndUpdate(ctx, filter, update, opts).Decode(&updated)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return domain.Task{}, err
+		}
+		return domain.Task{}, err
+	}
+	return updated, nil
 }
 
 // DeleteTask deletes a task from the database.
 // It takes a context.Context and a taskId string as parameters.
 // It returns an error if there was a problem deleting the task.
-func (tr *taskRepository) DeleteTask(ctx context.Context, taskId string) error{
+func (tr *taskRepository) DeleteTask(ctx context.Context, taskId string) error {
 	collection := tr.database.Collection(tr.collection)
 	_, err := collection.DeleteOne(context.TODO(), bson.M{"_id": taskId})
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	return nil
 }
-
-
-
-
-
---
+++ Repositories/task_repository_test.go
@@ -0,0 +1,191 @@
+package repositories
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	domain "example/go-clean-architecture/Domain"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var taskId = primitive.NewObjectID()
+var title = "New Task"
+var description = "This is a new task"
+var status = "Started"
+var due_date = time.Now().UTC().Truncate(24 * time.Hour)
+
+type TaskRepositoryTestSuite struct {
+	suite.Suite
+	client     *mongo.Client
+	db         *mongo.Database
+	collection string
+	repo       domain.TaskRepository
+}
+
+func (suite *TaskRepositoryTestSuite) SetupSuite() {
+	// Connect to the MongoDB test instance
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	suite.Require().NoError(err)
+
+	// Ping the MongoDB instance to ensure connection is established
+	err = client.Ping(context.Background(), nil)
+	suite.Require().NoError(err)
+
+	suite.client = client
+	suite.db = client.Database("testTaskManager")
+	suite.collection = "tasks"
+	suite.repo = NewTaskRepository(*suite.db, suite.collection)
+}
+
+func (suite *TaskRepositoryTestSuite) TearDownSuite() {
+	// Drop the test database after all tests have run
+	err := suite.db.Drop(context.Background())
+	suite.Require().NoError(err)
+
+	// Disconnect from MongoDB
+	err = suite.client.Disconnect(context.Background())
+	suite.Require().NoError(err)
+}
+
+func (suite *TaskRepositoryTestSuite) TestCreateTask() {
+	task := domain.Task{
+		Title:       title,
+		Description: description,
+		Status:      status,
+		DueDate:     due_date,
+	}
+
+	newtask, err := suite.repo.CreateTask(context.Background(), task)
+	suite.Require().NoError(err)
+
+	// Verify task wask inserted
+	collection := suite.db.Collection(suite.collection)
+	var result domain.Task
+
+	err = collection.FindOne(context.Background(), bson.M{"_id": newtask.ID}).Decode(&result)
+	suite.Require().NoError(err)
+	assert.Equal(suite.T(), task.Title, result.Title)
+	suite.NoError(err, "no error when creating and finding the same task")
+}
+
+func (suite *TaskRepositoryTestSuite) TestFindAlltasks() {
+	fetchedTasks, err := suite.repo.FindAlltasks(context.Background())
+	suite.Require().NoError(err)
+
+	// Verify the correct number of tasks were fetched
+	tasks := fetchedTasks[0]
+	// assert.Equal(suite.T(), 1, len(fetchedTasks))
+
+	
+	assert.Equal(suite.T(), title, tasks.Title, "Same task title  with the previously created task")
+	assert.Equal(suite.T(), description, tasks.Description, "Same task description with the previously created task")
+	assert.Equal(suite.T(), status, tasks.Status, "Same task status with the previously created task")
+	assert.Equal(suite.T(), due_date, tasks.DueDate, "Same task due_date with the previously created task")
+	
+
+}
+
+func (suite *TaskRepositoryTestSuite) TestFindTaskById() {
+	task := domain.Task{
+		Title: "New Task",
+		Description: "This is a new task",
+		Status: "Started",
+		DueDate: time.Now().UTC().Truncate(24 * time.Hour),
+	}
+	
+
+	newTask, err := suite.repo.CreateTask(context.Background(), task)
+	suite.Require().NoError(err)
+
+	foundTask, err := suite.repo.FindTaskById(context.Background(), newTask.ID.Hex())
+	suite.Require().NoError(err)
+
+	assert.Equal(suite.T(), task.Title, foundTask.Title, "Same task title with the previously created task")
+	assert.Equal(suite.T(), task.Description, foundTask.Description, "Same task description with the previously created task")
+	assert.Equal(suite.T(), task.Status, foundTask.Status, "Same task status with the previously created task")
+	assert.Equal(suite.T(), task.DueDate, foundTask.DueDate, "Same task due_date with the previously created task")
+}
+
+
+func (suite *TaskRepositoryTestSuite) TestUpdateTaskById() {
+	task := domain.Task{
+		Title: "New Task",
+		Description: "This is a new task",
+		Status: "Started",
+		DueDate: time.Now().UTC().Truncate(24 * time.Hour),
+	}
+	
+
+	newTask, err := suite.repo.CreateTask(context.Background(), task)
+	suite.Require().NoError(err)
+
+	newTitle := "Updated Task"
+	newDescription := "This is an updated task"
+	newStatus := "Completed"
+	newDueDate := time.Now().UTC().Truncate(24 * time.Hour)
+
+	taskUpdate := domain.Task{
+		Title:       newTitle,
+		Description: newDescription,
+		Status:      newStatus,
+		DueDate:    newDueDate,
+	}
+
+	task, err = suite.repo.UpdateTaskById(context.Background(), taskUpdate, newTask.ID.Hex())
+	suite.Require().NoError(err)
+
+	// find out if the task is updated
+	collection := suite.db.Collection(suite.collection)
+	var result domain.Task
+
+	err = collection.FindOne(context.Background(), bson.M{"_id": task.ID}).Decode(&result)
+	suite.Require().NoError(err)
+
+	// Check if its the same task
+	taskFound, err := suite.repo.FindTaskById(context.Background(), task.ID.Hex())
+	suite.Require().NoError(err)
+
+	// check if the fields are updated
+	assert.Equal(suite.T(), newTitle, taskFound.Title, "Same updated task title with the previously updated task")
+	assert.Equal(suite.T(), newDescription, taskFound.Description, "Same updated task description with the previously updated task")
+	assert.Equal(suite.T(), newStatus, taskFound.Status, "Same updated task status with the previously updated task")
+	assert.Equal(suite.T(), newDueDate, taskFound.DueDate, "Same updated task due_date with the previously updated task")
+}
+
+func (suite *TaskRepositoryTestSuite) TestDeleteTask() {
+	
+
+	// create a new task then remove it
+	task := domain.Task{
+		Title:       "new task delete",
+		Description: "This is a new task to be deleted",
+		Status:      "Not Started",
+		DueDate:    time.Now().UTC().Truncate(24 * time.Hour),
+	}
+
+	newTask, err := suite.repo.CreateTask(context.Background(), task)
+	suite.Require().NoError(err)
+
+	// Verify the task is inserted
+	collection := suite.db.Collection(suite.collection)
+	var result domain.Task
+
+	err = collection.FindOne(context.Background(), bson.M{"_id": newTask.ID}).Decode(&result)
+	suite.Require().NoError(err)
+
+	err = suite.repo.DeleteTask(context.Background(), newTask.ID.Hex())
+	suite.Require().NoError(err)
+
+}
+
+func TestTaskRepositoryTestSuite(t *testing.T) {
+	suite.Run(t, new(TaskRepositoryTestSuite))
+}
